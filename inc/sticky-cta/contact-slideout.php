2b6c3dc75fd88f5f8726801edb4a3ded388537f7
Rob Cakebread <rob.cakebread@artemis.marketing>
2021-07-28 14:56:58 +0100

    28.07.21

--- inc/sticky-cta/contact-slideout.php
+++ inc/sticky-cta/contact-slideout.php
@@ -6,10 +6,8 @@
 	<div class="art-side-form-inner">
 		<h3>Contact Us</h3><p>Please get in touch using the form below</p>
 		<div class="frm_forms" id="frm_form_15_container">
-
-    <?php //echo do_shortcode( '[contact-form-7 id="$form_id" title="Contact form 1"]' ); ?>
-	<?php echo do_shortcode('[gravityform id="2" title="false" ajax="true"]'); ?>
-	<?php  //echo FrmFormsController::get_form_shortcode(array('id'=> 2, 'title' => false, 'description' => false, 'ajax' => true)); ?>
-
+			<?php echo do_shortcode('[gravityform id="2" title="false" description="false" ajax="true"]'); ?>
+		</div>
+	<a href="javascript:void(0);" data-toggle="#art-side-form" class="close-cross"></a>
+	</div>
 </div>
-<a href="javascript:void(0);" data-toggle="#art-side-form" class="close-cross"></a></div></div>
